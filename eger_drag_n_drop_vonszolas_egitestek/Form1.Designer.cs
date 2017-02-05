a5f5af00b085ae0c3071eb7d5c5a4d2a78276e5a
Benedek Vass <vabe44@gmail.com>
2017-02-05 16:35:39 +0100

    Add project files.

---
+++ eger_drag_n_drop_vonszolas_egitestek/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace eger_drag_n_drop_vonszolas_egitestek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Ellenorzes = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::eger_drag_n_drop_vonszolas_egitestek.Properties.Resources.naprendszer;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(162, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Tag = "Venusz";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(277, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Tag = "Mars";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(492, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 27);
+            this.label3.TabIndex = 3;
+            this.label3.Tag = "Szaturnusz";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(661, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 27);
+            this.label4.TabIndex = 4;
+            this.label4.Tag = "Neptunusz";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(102, 342);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 27);
+            this.label5.TabIndex = 5;
+            this.label5.Tag = "Merkur";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(231, 333);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 27);
+            this.label6.TabIndex = 6;
+            this.label6.Tag = "Fold";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.Control;
+            this.label7.Location = new System.Drawing.Point(384, 333);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(123, 27);
+            this.label7.TabIndex = 7;
+            this.label7.Tag = "Jupiter";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.SystemColors.Control;
+            this.label8.Location = new System.Drawing.Point(600, 308);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 27);
+            this.label8.TabIndex = 8;
+            this.label8.Tag = "Uranusz";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.SystemColors.Control;
+            this.label9.Location = new System.Drawing.Point(677, 237);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(123, 27);
+            this.label9.TabIndex = 9;
+            this.label9.Tag = "Pluto";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Ellenorzes
+            // 
+            this.Ellenorzes.Location = new System.Drawing.Point(803, 379);
+            this.Ellenorzes.Name = "Ellenorzes";
+            this.Ellenorzes.Size = new System.Drawing.Size(127, 30);
+            this.Ellenorzes.TabIndex = 10;
+            this.Ellenorzes.Text = "Ellenorzes";
+            this.Ellenorzes.UseVisualStyleBackColor = true;
+            this.Ellenorzes.Click += new System.EventHandler(this.Ellenorzes_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(803, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 30);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Ujrakezdes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.SystemColors.Control;
+            this.label10.Location = new System.Drawing.Point(10, 192);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(123, 27);
+            this.label10.TabIndex = 12;
+            this.label10.Tag = "Nap";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 462);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Ellenorzes);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button Ellenorzes;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ eger_drag_n_drop_vonszolas_egitestek/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eger_drag_n_drop_vonszolas_egitestek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Label[] helyek = new Label[10];
+        Label[] egitestek = new Label[10];
+        private string[] egitestNevek = {"Fold", "Jupiter", "Mars", "Merkur", "Nap", "Neptunusz", "Pluto", "Szaturnusz", "Uranusz", "Venusz"};
+        private bool moving = false;
+        int aktX, aktY = 0;
+        private Label lbDragged;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                var ujCimke = new Label
+                {
+                    Width = 127,
+                    Height = 27,
+                    Left = 803,
+                    Top = 10 + i*30,
+                    BorderStyle = BorderStyle.FixedSingle,
+                    BackColor = Color.Gold,
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Text = egitestNevek[i]
+                };
+
+                egitestek[i] = ujCimke;
+                this.Controls.Add(ujCimke);
+
+                ujCimke.BringToFront();
+                ujCimke.MouseDown += egitest_Mousedown;
+                ujCimke.MouseUp += egitest_Mouseup;
+                ujCimke.MouseMove += egitest_Mousemove;
+            }
+
+            helyek[0] = label1;
+            helyek[1] = label2;
+            helyek[2] = label3;
+            helyek[3] = label4;
+            helyek[4] = label5;
+            helyek[5] = label6;
+            helyek[6] = label7;
+            helyek[7] = label8;
+            helyek[8] = label9;
+            helyek[9] = label10;
+        }
+
+        private void egitest_Mousemove(object sender, MouseEventArgs e)
+        {
+            if (moving)
+            {
+                lbDragged.Left += e.X - aktX;
+                lbDragged.Top += e.Y - aktY;
+                lbDragged.Tag = null;
+                lbDragged.BackColor = Color.White;
+            }
+        }
+
+        private void egitest_Mouseup(object sender, MouseEventArgs e)
+        {
+            this.Cursor = Cursors.Default;
+            moving = false;
+            for (int i = 0; i < helyek.Length; i++)
+            {
+                if (Tavolsag(lbDragged, helyek[i]) < 20)
+                {
+                    lbDragged.Left = helyek[i].Left;
+                    lbDragged.Top = helyek[i].Top;
+                    lbDragged.Tag = i;
+                    break;
+                }
+            }
+            lbDragged = null;
+        }
+
+        private void egitest_Mousedown(object sender, MouseEventArgs e)
+        {
+            this.Cursor = Cursors.Hand;
+            moving = true;
+            lbDragged = sender as Label;
+            aktX = e.X;
+            aktY = e.Y;
+            lbDragged?.BringToFront();
+        }
+
+        private void Ellenorzes_Click(object sender, EventArgs e)
+        {
+            foreach (var label in egitestek)
+            {
+                if (label.Tag != null && label.Text == helyek[Convert.ToInt32(label.Tag)].Tag.ToString())
+                {
+                    label.BackColor = Color.Lime;
+                }
+                else
+                {
+                    label.BackColor = Color.Red;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < egitestek.Length; i++)
+            {
+                egitestek[i].Left = 803;
+                egitestek[i].Top = 10 + i*30;
+                egitestek[i].Tag = null;
+                egitestek[i].BackColor = Color.White;
+            }
+        }
+
+        private double Tavolsag(Label cimke1, Label cimke2)
+        {
+            return Math.Sqrt(Math.Pow(cimke2.Left - cimke1.Left, 2) + Math.Pow(cimke2.Top - cimke1.Top, 2));
+        }
+    }
+}
